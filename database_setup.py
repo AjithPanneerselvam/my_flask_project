cfea10843bf551f99703338b396fd45d8bb942e5
AjithPanneerselvam <ajith.ark1@gmail.com>
2016-04-07 15:25:29 +0530

    initial

--- database_setup.py
+++ database_setup.py
@@ -1,20 +1,33 @@
+# Start of config code
 import sys
-from  sqlalchemy import Column, ForeignKey, Integer, String
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
+Base = declarative_base()
+#end of config code
+
+# Class definitions
 class Restaurant(base):
 	__tablename__ = 'restaurant'
+
+	name  = Column(String(80), nullable = False)
+	id = Column(Integer, primary_key = True)
 
 class MenuItem(base):
 	__tablename__ = 'menu_item'
 
+	id = Column(Integer, primary_key = True)
+	course = Column(String(250))
+	description = Column(String(250))
+	price = Column(String(8))
 
+	restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
+	restaurant = relationship(Restaurant)
 
-Base = declarative_base()
 # insert at end of file
-
 engine = create_engine('sqlite://restaurantmenu.db')
 
 Base.metadata.create_all(engine)
