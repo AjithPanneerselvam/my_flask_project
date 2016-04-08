10db2901845331212967c68c3b852de6687db4ff
AjithPanneerselvam <ajith.ark1@gmail.com>
2016-04-08 19:18:57 +0530

    server

--- database_setup.py
+++ database_setup.py
@@ -12,12 +12,14 @@ Base = declarative_base()
 class Restaurant(Base):
 	__tablename__ = 'restaurant'
 
+	#mapper starts
 	name  = Column(String(80), nullable = False)
 	id = Column(Integer, primary_key = True)
 
 class MenuItem(Base):
 	__tablename__ = 'menu_item'
 
+	#mapper starts
 	id = Column(Integer, primary_key = True)
 	course = Column(String(250))
 	description = Column(String(250))
